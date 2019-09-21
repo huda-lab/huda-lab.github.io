bbbe66f8213eabf428ccaece9186c2f423af6be6
Azza Abouzied <directorunixlabnyuad@gmail.com>
2019-09-21 14:37:56 +0400

    website update

---
+++ news.js
@@ -0,0 +1,20 @@
+var news =  [
+    {
+        headline: "Professor Azza Abouzied wins the Test of Time Award at VLDB 2019",
+        link: "https://nyuad.nyu.edu/en/academics/divisions/science/faculty/azza-abouzied/news/test-of-time-award.html",
+        abstract: "VLDB Test of Time Award paper is selected from the VLDB Conference from 10 to 12 years earlier that best meets the “test of time”. The committee evaluates the impact of the paper in practice, e.g., in products and services. Impact on the academic community demonstrated through significant follow-through research.",
+        date: "Aug 29, 2019",
+    },
+    {
+        headline: "Miro Mannino & Maeda Hanafi present their work at HILDA in Amsterdam",
+        link: "",
+        abstract: "",
+        date: "July 5, 2019",
+    },
+    {
+        headline: "",
+        link: "",
+        abstract: "",
+        date: "",
+    },
+]
---
+++ projects.js
@@ -0,0 +1,131 @@
+var projects = [
+    {
+        id: 14,
+        title: "Synner",
+        subtitle: "Generating Realistic Synthetic Data",
+        authors: ["Mino Mannino", "Azza Abouzied"],
+        members: [0, 3, 6, 7, 10, 14],
+        links: [
+            {
+                txt: "Paper @ UIST 2019",
+                ref: "http://azza.azurewebsites.net/files/Synner-UIST2019.pdf"
+            },
+            {
+                txt: "30s Video",
+                ref: "https://www.youtube.com/watch?v=ez2Tge5Bf2M"
+            },
+            {
+                txt: "Video",
+                ref: "https://www.youtube.com/watch?v=BH9tiuoayp0"
+            },
+            {
+                txt: "Live Demo",
+                ref: "http://Synner.io"
+            },
+            {
+                txt: "Git",
+                ref: ""
+            }
+        ],
+        abstract: "Synner is a tool that helps users generate real-looking synthetic data by visually and declaratively specifying the properties of the dataset such as each field’s statistical distribution, its domain, and its relationship to other fields. It provides instant feedback on every user interaction by updating multiple visualizations of the generated dataset and even suggests data generation specifications from a few user examples and interactions. Synner visually communicates the inherent randomness of statistical data generation. Our evaluation of Synner demonstrates its effectiveness at generating realistic data when compared with Mockaroo, a popular data generation tool, and with hired developers who coded data generation scripts for a fee.",
+        teaserpic: "images/projects/synner.png",
+        thumbnail: "images/projects/synner-thumb.png"
+    },
+    {
+        id: 13,
+        title: "Scalable Package Queries in Relational Database Systems",
+        subtitle: "Using Approximation techniques to scale and speed up package queries",
+        authors: ["Matteo Brucato", "Peter Haas", "Alexandra Meliou", "Azza Abouzied"],
+        members: [19, 0],
+        grants: ["NSF Award IIS-1420941", "NSF EAGER In-Database Prescriptive Analytics (2019)"],
+        award: "CACM 2019 Research Highlight Award | ACM SIGMOD Research Highlight Award | Best of VLDB 2016",
+        links: [
+            {
+                txt: "Paper @ CACM 2019",
+                ref: "https://cacm.acm.org/magazines/2019/2/234358-scalable-computation-of-high-order-optimization-queries/abstract"
+            },
+            {
+                txt: "Paper @ VLDBJ 2017",
+                ref: "https://link.springer.com/article/10.1007/s00778-017-0483-4"
+            },
+            {
+                txt: "Paper @ SIGMOD Record 2017",
+                ref: "http://dl.acm.org/citation.cfm?id=3093761"
+            },
+            {
+                txt: "Paper @ VLDB 2016",
+                ref: "http://arxiv.org/abs/1512.03564"
+            },
+            {
+                txt: "Git",
+                ref: ""
+            }
+        ]
+    },
+    {
+        id: 16,
+        title: "Texture",
+        subtitle: "A Collaborative Framework for Structure Identification over Print Documents",
+        authors: ["Maeda Hanafi", "Miro Mannino", "Azza Abouzied"],
+        members: [0, 1, 3, 8, 9, 11, 12, 13],
+        links: [
+            {
+                txt: "Paper @ HILDA 2019",
+                ref: "http://hilda.io/2019/proceedings/HILDA2019_paper_8.pdf"
+            },
+            {
+                txt: "Git",
+                ref: ""
+            }
+        ]
+    },
+
+    {
+        id: 12,
+        title: "Qetch",
+        subtitle: "Expressive Time Series Querying with Hand-Drawn Scale-Free Sketches",
+        authors: ["Miro Mannino", "Azza Abouzied"],
+        members: [0, 3],
+        award: "Best Paper Award (top 1%)",
+        links: [
+            {
+                txt: "Paper @ CHI 2018",
+                ref: "https://github.com/dtl-nyuad/qetch/raw/resources/CHI_18_QETCH.pdf"
+            },
+            {
+                txt: "Source Code & Data",
+                ref: "https://github.com/dtl-nyuad/qetch"
+            },
+            {
+                txt: "Video",
+                ref: "https://youtu.be/4YQTuUuIFbI"
+            },
+            {
+                txt: "Git",
+                ref: ""
+            }
+
+        ]
+    },
+    {
+        id: 11,
+        title: "SEER",
+        subtitle: "Auto-Generating Information Extraction Rules from User-Specified Examples.",
+        authors: ["Maeda Hanafi", "Azza Abouzied", "Laura Chiticariu", "Yunyao Li"],
+        members: [0, 1],
+        links: [
+            {
+                txt: "Paper @ CHI 2017",
+                ref: "https://doi.org/10.1145/3025453.3025540"
+            },
+            {
+                txt: "Video",
+                ref: "https://www.youtube.com/watch?v=pS18h5REC3o"
+            },
+            {
+                txt: "Demo @ SIGMOD 2017",
+                ref: "https://doi.org/10.1145/3035918.3056443"
+            }
+        ]
+    },
+];
