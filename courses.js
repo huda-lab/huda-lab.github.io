64e5b0ddbdcbb45c8753e6058ce94e38ee1a0353
Azza Abouzied <directorunixlabnyuad@gmail.com>
2019-09-22 20:20:08 +0400

    website update

---
+++ courses.js
@@ -0,0 +1,26 @@
+var courses=[
+{
+    id: 3,
+        title: "CORE X Data",
+    syllabus: "syllabi/Data-Syllabus-Abouzied-2016.pdf",
+    links: [
+    {
+        year: "Spring 2017",
+        ref: "https://piazza.com/new_york_university_abu_dhabi/spring2017/corexad3q001data/home"
+    }
+]
+
+},
+{
+    id: 4,
+        title: "Capstone on Systems for Interactive Data Analysis",
+    syllabus: "syllabi/Capstone-InteractiveSystems-SyllabusSpring2019.pdf",
+    links: [
+    {
+        year: "Spring 2019",
+        ref: "https://piazza.com/new_york_university_abu_dhabi/spring2019/sida00/home"
+    }
+]
+
+}
+];
--- news.js
+++ news.js
@@ -3,13 +3,13 @@ var news =  [
         headline: "Miro Mannino will be presenting Synner at UIST in New Orleans",
         link: "http://uist.acm.org/uist2019/program/#Day2",
         abstract: "",
-        date: "October 20, 2018",
+        date: "October 20, 2019",
     },
     {
         headline: "Professor Azza Abouzied wins the Test of Time Award at VLDB 2019",
         link: "https://nyuad.nyu.edu/en/academics/divisions/science/faculty/azza-abouzied/news/test-of-time-award.html",
         abstract: "VLDB Test of Time Award paper is selected from the VLDB Conference from 10 to 12 years earlier that best meets the “test of time”. The committee evaluates the impact of the paper in practice, e.g., in products and services. Impact on the academic community demonstrated through significant follow-through research.",
-        date: "Aug 29, 2019",
+        date: "August 29, 2019",
     },
     {
         headline: "Miro Mannino & Maeda Hanafi present their work at HILDA in Amsterdam",
@@ -24,7 +24,7 @@ var news =  [
         date: "June 13, 2018",
     },
     {
-        headline: "Qetch paper is awarded with the Best Paper Award",
+        headline: "Qetch paper wins Best Paper Award",
         link: "https://dl.acm.org/citation.cfm?id=3173962",
         abstract: "",
         date: "April 25, 2018",
