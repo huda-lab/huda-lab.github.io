a9d578b755e4e39d629baa9feb79aa9e8df9871a
Miro Mannino <miro.mannino@gmail.com>
2019-09-22 13:59:19 +0400

    updated new and thumb for qetch

--- news.js
+++ news.js
@@ -1,5 +1,11 @@
 var news =  [
     {
+        headline: "Miro Mannino will be presenting Synner at UIST in New Orleans",
+        link: "http://uist.acm.org/uist2019/program/#Day2",
+        abstract: "",
+        date: "October 20, 2018",
+    },
+    {
         headline: "Professor Azza Abouzied wins the Test of Time Award at VLDB 2019",
         link: "https://nyuad.nyu.edu/en/academics/divisions/science/faculty/azza-abouzied/news/test-of-time-award.html",
         abstract: "VLDB Test of Time Award paper is selected from the VLDB Conference from 10 to 12 years earlier that best meets the “test of time”. The committee evaluates the impact of the paper in practice, e.g., in products and services. Impact on the academic community demonstrated through significant follow-through research.",
@@ -7,9 +13,27 @@ var news =  [
     },
     {
         headline: "Miro Mannino & Maeda Hanafi present their work at HILDA in Amsterdam",
-        link: "",
+        link: "https://hilda.io/2019/",
         abstract: "",
         date: "July 5, 2019",
+    },
+    {
+        headline: "Miro Mannino presents Qetch as SIGMOD in Houston",
+        link: "https://sigmod2018.org/",
+        abstract: "",
+        date: "June 13, 2018",
+    },
+    {
+        headline: "Qetch paper is awarded with the Best Paper Award",
+        link: "https://dl.acm.org/citation.cfm?id=3173962",
+        abstract: "",
+        date: "April 25, 2018",
+    },
+    {
+        headline: "Miro Mannino presents Qetch at CHI in Montréal",
+        link: "https://dl.acm.org/citation.cfm?id=3173962",
+        abstract: "",
+        date: "April 23, 2018",
     },
     {
         headline: "",
