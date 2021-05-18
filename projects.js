6edf676e0a07259a8eb0b4a880f02d6e7b7dd9ee
Miro Mannino <miro.mannino@gmail.com>
2021-05-18 15:09:24 +0400

    epipolicy links changes

--- projects.js
+++ projects.js
@@ -189,6 +189,18 @@ var projects = [
                 txt: "Tech Report Available on Request",
                 ref: "mailto:azza@nyu.edu"
             },
+            {
+                txt: "Official Website",
+                ref: "https://epipolicy.github.io/"
+            },
+            {
+                txt: "Short Video",
+                ref: "https://www.youtube.com/watch?v=JDYb4_coAf4&ab_channel=HuDaLab-NYUAD"
+            },
+            {
+                txt: "Longer Video",
+                ref: "https://www.youtube.com/watch?v=KJ7-Wt5bEeM&ab_channel=HuDaLab-NYUAD"
+            }
         ],
         abstract: "EpiPolicy is a population-based epidemic simulator and policy aid that allows users to customize its compartmental model to capture different epidemic scenarios for epidemics like COVID-19. Its user interface makes it easy to define various interventions such as social distancing, school closure, vaccination, and disease-specific interventions. EpiPolicy aids policy makers by simulating and constructing locale-specific intervention schedules that reduce disease burden, and minimize social and economic costs.",
         teaserpic: "images/projects/epidemic-intervention.png",
